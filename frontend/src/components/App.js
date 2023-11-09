a2f041c17385fc9b0e5c87a69ca971fdcdebbf8b
Aleksandr Manujlo <dregmar@bk.ru>
2023-11-09 16:58:42 +0600

    add routing

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -2,7 +2,15 @@ import  '../styles.scss';
 import  'bootstrap';
 import React from 'react';
 import LoginPage from './LoginPage';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-const App = () => <LoginPage />;
+const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<LoginPage/>} />
+      <Route path='login' element={<LoginPage/>} />
+    </Routes>
+  </BrowserRouter>
+);
 
 export default App;
