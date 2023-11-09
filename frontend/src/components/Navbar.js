02057c8fb09d850c1e51d18154750a8dfeb2cf51
Aleksandr Manujlo <dregmar@bk.ru>
2023-11-09 21:31:48 +0600

    refactor Navbar.js

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import NavbarBrand from './NavbarBrand';
 
 
 const Navbar = () => (
   <nav className="shadow-sm navbar navbar-expand-lg navbar-light bg-white">
-    <NavbarBrand />
+    <div className="container">
+      <a className="navbar-brand" href="/">Gussenger</a>
+    </div>
   </nav>
 )
 
