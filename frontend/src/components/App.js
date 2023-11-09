39dd63cdf415f5da167ef831ebf87f4aa8e456e8
Aleksandr Manujlo <dregmar@bk.ru>
2023-11-09 21:30:49 +0600

    add PageNotFound

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -2,15 +2,17 @@ import  '../styles.scss';
 import  'bootstrap';
 import React from 'react';
 import LoginPage from './LoginPage';
+import PageNotFound from './PageNotFound';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const App = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<LoginPage/>} />
-      <Route path='login' element={<LoginPage/>} />
-    </Routes>
-  </BrowserRouter>
+    <BrowserRouter>
+        <Routes>
+          <Route path='*' element={<PageNotFound/>} />
+          <Route path='/' element={<LoginPage/>} />
+          <Route path='login' element={<LoginPage/>} />
+        </Routes>
+    </BrowserRouter>
 );
 
 export default App;
---
+++ frontend/src/components/PageNotFound.js
@@ -0,0 +1,31 @@
+import Navbar from './Navbar';
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import pageNotFound from '../images/404.png';
+
+
+const PageNotFound = () => {
+  const { t } = useTranslation();
+  return (
+    <div className="h-100">
+      <div className="h-100" id="chat">
+        <div className="d-flex flex-column h-100">
+          <Navbar />
+          <div className="text-center">
+            <img
+              alt="Страница не найдена"
+              className="img-fluid h-25"
+              src={pageNotFound}
+            />
+            <h1 className="h4 text-muted">{t('pageNotFound.pageNotFound')}</h1>
+            <p className="text-muted">{t('pageNotFound.youCanGo')}</p>
+              <a href="/">{t('pageNotFound.toMainPage')}</a>
+          </div>
+        </div>
+        <div className="Toastify"></div>
+      </div>
+    </div>
+  )
+}
+
+export default PageNotFound;
