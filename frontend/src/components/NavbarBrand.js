db625a5f54d7da6eddef3f8aeb2a34e27cfed466
Aleksandr Manujlo <dregmar@bk.ru>
2023-11-09 21:29:24 +0600

    move NavbarBrand to Navbar.js

--- frontend/src/components/NavbarBrand.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const NavbarBrand = () => (
-  <div className="container">
-    <a className="navbar-brand" href="/">Gussenger</a>
-  </div>
-);
-
-export default NavbarBrand;
