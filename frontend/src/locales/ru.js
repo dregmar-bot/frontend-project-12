7a0a37b50524f6f46b1c8fd753cbd6ed2d9d822e
Aleksandr Manujlo <dregmar@bk.ru>
2023-11-14 18:10:23 +0600

    add texts for new components

--- frontend/src/locales/ru.js
+++ frontend/src/locales/ru.js
@@ -10,11 +10,26 @@ export default {
         haveNoAccount: 'Нет аккаунта?',
         registration: 'Регистрация',
       },
+      errors: {
+        ERR_BAD_REQUEST: 'Неверные имя пользователя или пароль',
+        ERR_BAD_RESPONSE: 'Ошибка сети. Попробуйте позже',
+      }
     },
     pageNotFound: {
       pageNotFound: 'Страница не найдена',
       youCanGo: 'Но вы можете перейти',
       toMainPage: 'на главную страницу',
+    },
+    chatPage: {
+      channelBox: {
+        channels: 'Каналы',
+
+      },
+      messagesBox: {
+        key_one: '{{count}} сообщение',
+        key_few: '{{count}} сообщения',
+        key_many: '{{count}} сообщений',
+      }
     }
   }
 };
