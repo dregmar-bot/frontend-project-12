cd21ed403460d01e324c26d9f72d099b7b7a7351
Aleksandr Manujlo <dregmar@bk.ru>
2023-11-14 18:07:15 +0600

    refactor Navbar.js

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import UserContext from '../contexts/index.js';
 
 
-const Navbar = () => (
-  <nav className="shadow-sm navbar navbar-expand-lg navbar-light bg-white">
-    <div className="container">
-      <a className="navbar-brand" href="/">Gussenger</a>
-    </div>
-  </nav>
-)
+const Navbar = () => {
+  const { activeUser, isAuthorized } = useContext(UserContext);
+  console.log(activeUser)
+
+  return (
+    <nav className="shadow-sm navbar navbar-expand-lg navbar-light bg-white">
+      <div className="container">
+        <a className="navbar-brand" href="/">Goossenger</a>
+        { isAuthorized() ? <button type="button" className="btn btn-primary">Выйти</button> : null}
+      </div>
+    </nav>
+  )
+}
 
 export default Navbar;
