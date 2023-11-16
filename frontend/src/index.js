10d6cb22851fb1b90133deee7a47b2881b6793ff
Aleksandr Manujlo <dregmar@bk.ru>
2023-11-16 22:45:37 +0600

    refactor index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,21 +1,10 @@
 import ReactDOM from 'react-dom/client';
 import React from 'react';
-import App from './components/App';
-import './i18n'
-import UserProvider from './providers/UserProvider';
-import StoreProvider from './providers/StoreProvider';
+import init from './init';
 
 const app = async () => {
   const root = ReactDOM.createRoot(document.getElementById('root'));
-  root.render(
-      <React.StrictMode>
-        <UserProvider>
-          <StoreProvider>
-            <App />
-          </StoreProvider>
-        </UserProvider>
-      </React.StrictMode>
-  );
+  root.render( await init());
 };
 
 app();
