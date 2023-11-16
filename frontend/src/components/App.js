f55626ae23ecda4be3d1ea96c92dba73006b948a
Aleksandr Manujlo <dregmar@bk.ru>
2023-11-16 22:47:37 +0600

    add socket event subscribes to app

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,19 +1,34 @@
 import  '../styles.scss';
 import  'bootstrap';
-import React from 'react';
+import React, { useContext } from 'react';
+import { addChannel } from '../slices/channels';
+import { addMessage } from '../slices/messages';
 import LoginPage from './LoginPage';
 import PageNotFound from './PageNotFound';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ChatPage from "./ChatPage";
+import SocketContext from '../contexts/socketContext';
+import { useDispatch } from 'react-redux';
 
-const App = () => (
+const App = () => {
+  const dispatch = useDispatch();
+  const { socket } = useContext(SocketContext);
+  socket.on('newMessage', (payload) => {
+    dispatch(addMessage(payload));
+  });
+  socket.on('newChannel', (payload) => {
+    dispatch(addChannel(payload))
+  });
+
+  return (
     <BrowserRouter>
-        <Routes>
-          <Route path='*' element={<PageNotFound/>} />
-          <Route path='/' element={<ChatPage/>} />
-          <Route path='login' element={<LoginPage/>} />
-        </Routes>
+      <Routes>
+        <Route path='*' element={<PageNotFound/>} />
+        <Route path='/' element={<ChatPage/>} />
+        <Route path='login' element={<LoginPage/>} />
+      </Routes>
     </BrowserRouter>
-);
+  );
+}
 
 export default App;
