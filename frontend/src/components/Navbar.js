50d43876608ff965e1fb28d487ff041dc2e2f86b
Aleksandr Manujlo <dregmar@bk.ru>
2023-11-23 19:18:12 +0600

    refactor Navbar.js

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,13 +1,27 @@
-import React from 'react';
-
+import React, { useContext } from 'react';
+import userContext from '../contexts/userContext';
+import { useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
-  const isAuthorized = () => localStorage.userToken ?? false;
+  const isAuthorized = () => !!localStorage.getItem('activeUser');
+  const { setUser } = useContext(userContext);
+  const navigate = useNavigate();
+
+  const handleUnauthorize = () => {
+    setUser({
+      username: null,
+      password: null,
+      token: null,
+    });
+    localStorage.clear();
+    navigate('/login')
+  }
+
   return (
     <nav className="shadow-sm navbar navbar-expand-lg navbar-light bg-white">
       <div className="container">
         <a className="navbar-brand" href="/">Goossenger</a>
-        { isAuthorized() ? <button type="button" className="btn btn-primary">Выйти</button> : null}
+        { isAuthorized() ? <button type="button" className="btn btn-primary" onClick={handleUnauthorize}>Выйти</button> : null}
       </div>
     </nav>
   )
