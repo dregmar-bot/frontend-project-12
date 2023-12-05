5bacb283d448add7ed1078594de1c7c756ae96c8
Aleksandr Manujlo <dregmar@bk.ru>
2023-12-05 14:17:47 +0600

    refactor SocketProvider.js

--- frontend/src/providers/SocketProvider.js
+++ frontend/src/providers/SocketProvider.js
@@ -3,25 +3,13 @@ import SocketContext from '../contexts/socketContext';
 
 const SocketProvider = ({ socket, children }) => {
 
-  const sendMessage = (message) => socket.emit('newMessage', message, (response) => {
-    if (response.status !== 'ok') {
-      throw new Error('Your message was not delivery');
-    }
-  });
+  const sendMessage = (message) => socket.timeout(5000).emitWithAck('newMessage', message);
 
-  const addChannel = async (channel) => await socket.timeout(5000).emitWithAck('newChannel', channel);
+  const addChannel = (channel) => socket.timeout(5000).emitWithAck('newChannel', channel);
 
-  const renameChannel = (id, name) => socket.emit('renameChannel', { id, name}, (response) => {
-    if (response.status !== 'ok') {
-      throw new Error('Your channel was not renamed');
-    }
-  });
+  const renameChannel = (id, name) => socket.timeout(5000).emitWithAck('renameChannel', { id, name});
 
-  const removeChannel = (id) => socket.emit('removeChannel', { id }, (response) => {
-    if (response.status !== 'ok') {
-      throw new Error('Your channel was not removed');
-    }
-  })
+  const removeChannel = (id) => socket.timeout(5000).emitWithAck('removeChannel', { id });
 
   return (
     <SocketContext.Provider value={{ sendMessage, addChannel, removeChannel, renameChannel }}>
