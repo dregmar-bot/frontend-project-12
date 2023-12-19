1f91ea09874be6290c25a513de5076c25eb239df
Aleksandr Manujlo <dregmar@bk.ru>
2023-12-19 16:06:49 +0600

    refactor LoginPage.js

--- frontend/src/components/LoginPage.js
+++ frontend/src/components/LoginPage.js
@@ -1,24 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import LoginCard from "./LoginCard";
 import Navbar from './Navbar';
+import { useNavigate } from 'react-router-dom';
 
-const LoginPage = () => (
-  <div className="h-100">
-    <div className="h-100" id="chat">
-      <div className="d-flex flex-column h-100">
-        <Navbar />
-        <div className="container-fluid h-100">
-          <div className="row justify-content-center align-content-center h-100">
-            <div className="col-12 col-md-8 col-xxl-6">
-              <LoginCard />
+const LoginPage = () => {
+  const token = localStorage.getItem('token');
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (token) {
+      navigate('/');
+    }
+  })
+  return (
+    <div className="h-100">
+      <div className="h-100" id="chat">
+        <div className="d-flex flex-column h-100">
+          <Navbar />
+          <div className="container-fluid h-100">
+            <div className="row justify-content-center align-content-center h-100">
+              <div className="col-12 col-md-8 col-xxl-6">
+                <LoginCard />
+              </div>
             </div>
           </div>
         </div>
+        <div className="Toastify"></div>
       </div>
-      <div className="Toastify"></div>
     </div>
-  </div>
-);
+  );
+};
 
 export default LoginPage;
 
