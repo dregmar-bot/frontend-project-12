0fe9196afe0f6011ed59f62d712f616295435b5f
Aleksandr Manujlo <dregmar@bk.ru>
2023-12-19 16:06:31 +0600

    refactor init.jsx

--- frontend/src/init.jsx
+++ frontend/src/init.jsx
@@ -1,6 +1,6 @@
 import i18next from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import resources from './locales/index.js';
+import ru from './locales/ru';
 import React from 'react';
 import App from './components/App';
 import UserProvider from './providers/UserProvider';
@@ -47,7 +47,9 @@ const init = async () => {
   await i18n
     .use(initReactI18next)
     .init({
-      resources,
+      resources: {
+        ru,
+      },
       fallbackLng: 'ru',
       interpolation: {
         escapeValue: false
