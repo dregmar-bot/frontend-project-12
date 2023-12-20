6d195e673b31fb50892c79e5faa12ea847732314
Aleksandr Manujlo <dregmar@bk.ru>
2023-12-20 15:58:26 +0600

    refactor SocketProvider.js

--- frontend/src/providers/SocketProvider.js
+++ frontend/src/providers/SocketProvider.js
@@ -2,10 +2,10 @@ import React, { useMemo } from 'react';
 import SocketContext from '../contexts/socketContext';
 
 const SocketProvider = ({ socket, children }) => {
-  const sendMessage = useMemo(() => (message) => socket.timeout(5000).emitWithAck('newMessage', message), []);
-  const addChannel = useMemo(() => (channel) => socket.timeout(5000).emitWithAck('newChannel', channel), []);
-  const renameChannel = useMemo(() => (id, name) => socket.timeout(5000).emitWithAck('renameChannel', { id, name }), []);
-  const removeChannel = useMemo(() => (id) => socket.timeout(5000).emitWithAck('removeChannel', { id }), []);
+  const sendMessage = useMemo(() => (message) => socket.timeout(5000).emitWithAck('newMessage', message), [socket]);
+  const addChannel = useMemo(() => (channel) => socket.timeout(5000).emitWithAck('newChannel', channel), [socket]);
+  const renameChannel = useMemo(() => (id, name) => socket.timeout(5000).emitWithAck('renameChannel', { id, name }), [socket]);
+  const removeChannel = useMemo(() => (id) => socket.timeout(5000).emitWithAck('removeChannel', { id }), [socket]);
 
   return (
     <SocketContext.Provider value={{
