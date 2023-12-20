99e64c5194d22a1dbce74f326af0ef029e8b1fbe
Aleksandr Manujlo <dregmar@bk.ru>
2023-12-20 16:50:25 +0600

    refactor SocketProvider.js

--- frontend/src/providers/SocketProvider.js
+++ frontend/src/providers/SocketProvider.js
@@ -1,17 +1,26 @@
-import React, { useMemo } from 'react';
+import React, { useCallback } from 'react';
 import SocketContext from '../contexts/socketContext';
 
 const SocketProvider = ({ socket, children }) => {
-  const sendMessage = useMemo(() => (message) => socket.timeout(5000).emitWithAck('newMessage', message), [socket]);
-  const addChannel = useMemo(() => (channel) => socket.timeout(5000).emitWithAck('newChannel', channel), [socket]);
-  const renameChannel = useMemo(() => (id, name) => socket.timeout(5000).emitWithAck('renameChannel', { id, name }), [socket]);
-  const removeChannel = useMemo(() => (id) => socket.timeout(5000).emitWithAck('removeChannel', { id }), [socket]);
+  const sendMessage = (message) => socket.timeout(5000).emitWithAck('newMessage', message);
+  const addChannel = (channel) => socket.timeout(5000).emitWithAck('newChannel', channel);
+  const renameChannel = (id, name) => socket.timeout(5000).emitWithAck('renameChannel', { id, name });
+  const removeChannel = (id) => socket.timeout(5000).emitWithAck('removeChannel', { id });
+
+  const memoizedSendMessage = useCallback(sendMessage, []);
+  const memoizedAddChannel = useCallback(addChannel, []);
+  const memoizedRenameChannel = useCallback(renameChannel, []);
+  const memoizedRemoveChannel = useCallback(removeChannel, []);
+
+  const api = {
+    sendMessage: memoizedSendMessage,
+    addChannel: memoizedAddChannel,
+    removeChannel: memoizedRemoveChannel,
+    renameChannel: memoizedRenameChannel,
+  };
 
   return (
-    <SocketContext.Provider value={{
-      sendMessage, addChannel, removeChannel, renameChannel,
-    }}
-    >
+    <SocketContext.Provider value={api}>
       { children }
     </SocketContext.Provider>
   );
