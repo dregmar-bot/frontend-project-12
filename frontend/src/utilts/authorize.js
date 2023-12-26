6c3dcf0b2605e440dd6afb5800ec296046a2ff6d
Aleksandr Manujlo <dregmar@bk.ru>
2023-12-26 18:34:09 +0600

    delete authorize.js

--- frontend/src/utilts/authorize.js
+++
@@ -1,9 +0,0 @@
-const authorize = (user, cb) => {
-  cb(user);
-  const { username, password, token } = user;
-  localStorage.setItem('username', username);
-  localStorage.setItem('password', password);
-  localStorage.setItem('token', token);
-};
-
-export default authorize;
