3a04ef96215efaa2da677b4d012b7c9c9970cc11
Aleksandr Manujlo <dregmar@bk.ru>
2023-12-30 16:49:29 +0600

    refactor AuthProvider.js

--- frontend/src/providers/AuthProvider.js
+++ frontend/src/providers/AuthProvider.js
@@ -4,32 +4,35 @@ import AuthContext from '../contexts/authContext';
 const AuthProvider = ({ children }) => {
   const [activeUser, setUser] = useState({
     username: null,
-    password: null,
     token: null,
   });
-  const isAuthorized = !!activeUser.token;
+  const isAuthorized = () => !!localStorage.getItem('chatToken');
+
   const authorize = (user) => {
     setUser(user);
-    const { username, password, token } = user;
-    localStorage.setItem('username', username);
-    localStorage.setItem('password', password);
-    localStorage.setItem('token', token);
+    const { username, token } = user;
+    localStorage.setItem('chatToken', token);
+    localStorage.setItem('chatUsername', username);
   };
   const deauthorize = () => {
     setUser({
       username: null,
-      password: null,
       token: null,
     });
-    localStorage.clear();
+    localStorage.removeItem('chatToken');
+    localStorage.removeItem('chatUsername');
   };
+  const getToken = () => localStorage.getItem('chatToken');
+  const getUsername = () => localStorage.getItem('chatUsername');
 
   const authApi = useMemo(() => ({
     activeUser,
     authorize,
     deauthorize,
     isAuthorized,
-  }), [activeUser, isAuthorized]);
+    getToken,
+    getUsername,
+  }), [activeUser]);
 
   return (
     <AuthContext.Provider value={authApi}>
