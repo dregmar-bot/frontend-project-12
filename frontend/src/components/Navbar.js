26f077916ceee7c9420683d7159a42ed58a10614
Aleksandr Manujlo <dregmar@bk.ru>
2024-01-03 17:30:07 +0600

    refactor Navbar.js

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,21 +1,22 @@
 import React, { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import AuthContext from '../contexts/authContext';
+import routes from '../routes';
 
 const Navbar = () => {
-  const { isAuthorized, deauthorize } = useContext(AuthContext);
+  const { activeUser, deauthorize } = useContext(AuthContext);
   const navigate = useNavigate();
 
   const handleEscape = () => {
     deauthorize();
-    navigate('/login');
+    navigate(routes.loginPath());
   };
 
   return (
     <nav className="shadow-sm navbar navbar-expand-lg navbar-light bg-white">
       <div className="container">
-        <a className="navbar-brand" href="/">Hexlet Chat</a>
-        { isAuthorized() ? <button type="button" className="btn btn-primary" onClick={handleEscape}>Выйти</button> : null}
+        <a className="navbar-brand" href={routes.chatPath()}>Hexlet Chat</a>
+        { activeUser ? <button type="button" className="btn btn-primary" onClick={handleEscape}>Выйти</button> : null}
       </div>
     </nav>
   );
