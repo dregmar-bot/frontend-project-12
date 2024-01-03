3d9bc3aff3c14209331cc0e5a93c28eb51ede987
Aleksandr Manujlo <dregmar@bk.ru>
2024-01-03 17:29:08 +0600

    add routes.js

---
+++ frontend/src/routes.js
@@ -0,0 +1,12 @@
+const routes = {
+  serverApi: {
+    dataPath: () => 'api/v1/data',
+    loginPath: () => '/api/v1/login',
+    signupPath: () => '/api/v1/signup',
+  },
+  chatPath: () => '/',
+  loginPath: () => '/login',
+  signupPath: () => '/signup',
+}
+
+export default routes;
