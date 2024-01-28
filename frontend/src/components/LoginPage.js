c6c740111c32dd8a522d2c267bb66b677f4572a7
Aleksandr Manujlo <dregmar@bk.ru>
2024-01-28 13:15:00 +0600

    refactor LoginPage.js

--- frontend/src/components/LoginPage.js
+++ frontend/src/components/LoginPage.js
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import LoginCard from './LoginCard';
-import Navbar from './Navbar';
+import NavigationBar from './NavigationBar';
 import AuthContext from '../contexts/authContext';
 import routes from '../routes';
 
@@ -17,7 +17,7 @@ const LoginPage = () => {
     <div className="h-100">
       <div className="h-100" id="chat">
         <div className="d-flex flex-column h-100">
-          <Navbar />
+          <NavigationBar />
           <div className="container-fluid h-100">
             <div className="row justify-content-center align-content-center h-100">
               <div className="col-12 col-md-8 col-xxl-6">
