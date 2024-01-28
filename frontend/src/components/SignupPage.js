7d99389b5bf5d347a80ec9971f304ce16a5e2630
Aleksandr Manujlo <dregmar@bk.ru>
2024-01-28 13:16:25 +0600

    refactor SignupPage.js

--- frontend/src/components/SignupPage.js
+++ frontend/src/components/SignupPage.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import Navbar from './Navbar';
+import NavigationBar from './NavigationBar';
 import SignupCard from './SignupCard';
 
 const SignupPage = () => (
   <div className="h-100">
     <div className="h-100" id="chat">
       <div className="d-flex flex-column h-100">
-        <Navbar />
+        <NavigationBar />
         <div className="container-fluid h-100">
           <div className="row justify-content-center align-content-center h-100">
             <div className="col-12 col-md-8 col-xxl-6">
