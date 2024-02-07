de10c6dae5cf578195288ff4797ea7fa75cc48ff
Aleksandr Manujlo <dregmar@bk.ru>
2024-02-07 18:17:51 +0400

    refactor App.js

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -2,6 +2,7 @@ import '../styles.scss';
 import 'bootstrap';
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import routes from '../routes';
 import LoginPage from './LoginPage';
 import PageNotFound from './PageNotFound';
 import ChatPage from './ChatPage';
@@ -10,10 +11,10 @@ import SignupPage from './SignupPage';
 const App = () => (
   <BrowserRouter>
     <Routes>
-      <Route path="*" element={<PageNotFound />} />
-      <Route path="/" element={<ChatPage />} />
-      <Route path="login" element={<LoginPage />} />
-      <Route path="signup" element={<SignupPage />} />
+      <Route path={routes.undefinedPath()} element={<PageNotFound />} />
+      <Route path={routes.chatPath()} element={<ChatPage />} />
+      <Route path={routes.loginPath()} element={<LoginPage />} />
+      <Route path={routes.signupPath()} element={<SignupPage />} />
     </Routes>
   </BrowserRouter>
 );
