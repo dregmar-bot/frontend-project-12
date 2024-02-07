e7e2ba0e166cfbd61282071cc05d98a1cbfe2013
Aleksandr Manujlo <dregmar@bk.ru>
2024-02-07 18:17:42 +0400

    refactor AddChannelModal.js

--- frontend/src/components/modals/AddChannelModal.js
+++ frontend/src/components/modals/AddChannelModal.js
@@ -2,21 +2,27 @@ import React, {
   useState, useContext, useEffect, useRef,
 } from 'react';
 import * as Yup from 'yup';
+import filter from 'leo-profanity';
 import { useTranslation } from 'react-i18next';
 import { ErrorMessage, Field, Form, Formik } from 'formik';
 import { useSelector, useDispatch } from 'react-redux';
 import { Modal, Button } from 'react-bootstrap';
 import { toast } from 'react-toastify';
-import { channelsSelectors, switchChannel } from '../../slices/channels';
+import { channelsSelectors } from '../../slices/channels';
+import { switchChannel, closeModal } from '../../slices/ui';
 import ApiContext from '../../contexts/apiContext';
 
-const AddChannelModal = ({ show, close }) => {
+const AddChannelModal = () => {
   const [isLoading, setLoading] = useState(false);
   const { addChannel } = useContext(ApiContext);
   const inputEl = useRef(null);
   const dispatch = useDispatch();
   const { t } = useTranslation();
   const channelNames = useSelector(channelsSelectors.selectAll).map((channel) => channel.name);
+  const isModalOpen = useSelector((state) => state.ui.modalsState.isOpen);
+  const modalType = useSelector((state) => state.ui.modalsState.modalType);
+
+  const showModal = isModalOpen && modalType === 'add';
 
   const channelNameSchema = Yup.object({
     name: Yup.string()
@@ -33,10 +39,10 @@ const AddChannelModal = ({ show, close }) => {
     if (inputEl.current) {
       inputEl.current.focus();
     }
-  }, [show]);
+  }, [showModal]);
 
   return (
-    <Modal show={show} onHide={close} centered>
+    <Modal show={showModal} onHide={() => dispatch(closeModal())} centered>
       <Modal.Header closeButton>
         <Modal.Title>{t('modals.channelModal.add')}</Modal.Title>
       </Modal.Header>
@@ -48,11 +54,12 @@ const AddChannelModal = ({ show, close }) => {
           validateOnChange={false}
           onSubmit={async ({ name }) => {
             try {
-              const { data } = await addChannel({ name });
+              const filteredName = filter.clean(name);
+              const { data } = await addChannel({ name: filteredName });
               toast.success(t('modals.toast.add'));
               setLoading(false);
               dispatch(switchChannel(data.id));
-              close();
+              dispatch(closeModal());
             } catch {
               toast.error(t('socketErrors.timeout'));
               setLoading(false);
@@ -77,7 +84,7 @@ const AddChannelModal = ({ show, close }) => {
                 />
               </div>
               <div className="d-flex justify-content-end">
-                <Button variant="secondary" className="me-2" onClick={close}>{t('modals.channelModal.cancel')}</Button>
+                <Button variant="secondary" className="me-2" onClick={() => dispatch(closeModal())}>{t('modals.channelModal.cancel')}</Button>
                 <Button variant="primary" type="submit" disabled={isLoading}>
                   {!isLoading ? t('modals.channelModal.submit') : t('modals.channelModal.sending')}
                 </Button>
