18770cc1625cde857c5afd62ef4a5629d759622d
Aleksandr Manujlo <dregmar@bk.ru>
2024-02-09 10:54:55 +0400

    refactor ChannelBox.js

--- frontend/src/components/ChannelBox.js
+++ frontend/src/components/ChannelBox.js
@@ -3,7 +3,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { Dropdown, Button, ButtonGroup, Nav } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
 import { channelsSelectors } from '../slices/channels';
-import { switchChannel, setEditingChannel, setModalType, openModal } from '../slices/ui';
+import { switchChannel, openModal } from '../slices/ui';
 import AddChannelModal from './modals/AddChannelModal';
 import RemoveChannelModal from './modals/RemoveChannelModal';
 import RenameChannelModal from './modals/RenameChannelModal';
@@ -14,21 +14,8 @@ const ChannelBox = () => {
   const channelId = useSelector((state) => state.ui.currentChannel);
   const channels = useSelector(channelsSelectors.selectAll);
 
-  const handleCreateChannel = () => {
-    dispatch(openModal());
-    dispatch(setModalType('add'));
-  };
-
-  const handleRenameChannel = (e) => {
-    dispatch(openModal());
-    dispatch(setEditingChannel(Number(e.target.id)));
-    dispatch(setModalType('rename'));
-  };
-
-  const handleRemoveChannel = (e) => {
-    dispatch(openModal());
-    dispatch(setEditingChannel(Number(e.target.id)));
-    dispatch(setModalType('remove'));
+  const handleOpenModal = (type, e) => {
+    dispatch(openModal({ type, channel: Number(e.target.id)}))
   };
 
   const list = channels.map((channel) => {
@@ -74,8 +61,8 @@ const ChannelBox = () => {
           </Dropdown.Toggle>
 
           <Dropdown.Menu>
-            <Dropdown.Item id={channel.id} href="#" onClick={handleRemoveChannel}>{t('chatPage.channelBox.remove')}</Dropdown.Item>
-            <Dropdown.Item id={channel.id} href="#" onClick={handleRenameChannel}>{t('chatPage.channelBox.rename')}</Dropdown.Item>
+            <Dropdown.Item id={channel.id} href="#" onClick={(e) => handleOpenModal('remove', e)}>{t('chatPage.channelBox.remove')}</Dropdown.Item>
+            <Dropdown.Item id={channel.id} href="#" onClick={(e) => handleOpenModal('rename', e)}>{t('chatPage.channelBox.rename')}</Dropdown.Item>
           </Dropdown.Menu>
         </Dropdown>
       </Nav.Item>
@@ -86,7 +73,7 @@ const ChannelBox = () => {
     <div className="col-4 col-md-2 border-end px-0 bg-light flex-column h-100 d-flex">
       <div className="d-flex mt-1 justify-content-between mb-2 ps-4 pe-2 p-4">
         <b>{t('chatPage.channelBox.channels')}</b>
-        <Button className="p-0 text-primary btn-group-vertical" variant="link" onClick={handleCreateChannel}>
+        <Button className="p-0 text-primary btn-group-vertical" variant="link" onClick={(e) => handleOpenModal('add', e)}>
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="21" height="21" fill="currentColor">
             <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
             <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
