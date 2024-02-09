0019820757efefd3d0ac903be0c05cc7e7a08cd5
Aleksandr Manujlo <dregmar@bk.ru>
2024-02-09 11:16:15 +0400

    refactor NavigationBar.js

--- frontend/src/components/NavigationBar.js
+++ frontend/src/components/NavigationBar.js
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 import { Button, Navbar } from 'react-bootstrap';
 import AuthContext from '../contexts/authContext';
 import routes from '../routes';
@@ -7,6 +8,7 @@ import routes from '../routes';
 const NavigationBar = () => {
   const { activeUser, deauthorize } = useContext(AuthContext);
   const navigate = useNavigate();
+  const { t } = useTranslation();
 
   const handleEscape = () => {
     deauthorize();
@@ -16,8 +18,8 @@ const NavigationBar = () => {
   return (
     <Navbar expand="lg" bg="white" className="shadow-sm">
       <div className="container">
-        <Link to={routes.chatPath()} className="navbar-brand">Hexlet Chat</Link>
-        { activeUser ? <Button variant="primary" onClick={handleEscape}>Выйти</Button> : null}
+        <Link to={routes.chatPath()} className="navbar-brand">{t('navigationBar.linkToMain')}</Link>
+        { activeUser ? <Button variant="primary" onClick={handleEscape}>{t('navigationBar.logout')}</Button> : null}
       </div>
     </Navbar>
   );
