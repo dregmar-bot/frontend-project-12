9e519fb7ee08cd5a5784981a47eb03061f518690
Aleksandr Manujlo <dregmar@bk.ru>
2024-02-12 12:27:17 +0400

    refactor App.js

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -3,20 +3,21 @@ import 'bootstrap';
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import routes from '../routes';
+import PrivateRoute from './PrivateRoute';
 import LoginPage from './LoginPage';
 import PageNotFound from './PageNotFound';
-import ChatPage from './ChatPage';
 import SignupPage from './SignupPage';
 
-const App = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route path={routes.undefinedPath()} element={<PageNotFound />} />
-      <Route path={routes.chatPath()} element={<ChatPage />} />
-      <Route path={routes.loginPath()} element={<LoginPage />} />
-      <Route path={routes.signupPath()} element={<SignupPage />} />
-    </Routes>
-  </BrowserRouter>
-);
-
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={routes.undefinedPath()} element={<PageNotFound />} />
+        <Route path={routes.chatPath()} element={<PrivateRoute />} />
+        <Route path={routes.loginPath()} element={<LoginPage />} />
+        <Route path={routes.signupPath()} element={<SignupPage />} />
+      </Routes>
+    </BrowserRouter>
+  )
+};
 export default App;
