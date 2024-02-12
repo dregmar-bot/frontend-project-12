fcde7633e2c365fdcddb6a99ac0501055b0bf5b9
Aleksandr Manujlo <dregmar@bk.ru>
2024-02-12 13:14:54 +0400

    refactor App.js

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,19 +1,35 @@
 import '../styles.scss';
 import 'bootstrap';
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React, { useContext } from 'react';
+import {BrowserRouter, Routes, Route, useLocation, Navigate} from 'react-router-dom';
+import AuthContext from '../contexts/authContext.js';
 import routes from '../routes';
-import PrivateRoute from './PrivateRoute';
 import LoginPage from './LoginPage';
 import PageNotFound from './PageNotFound';
 import SignupPage from './SignupPage';
+import ChatPage from "./ChatPage";
 
 const App = () => {
+
+  const PrivateRoute = ({ children }) => {
+    const { activeUser } = useContext(AuthContext);
+    const location = useLocation();
+
+    return (
+      activeUser ? children : <Navigate to={routes.loginPath()} state={{ from: location }} />
+    );
+  };
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path={routes.undefinedPath()} element={<PageNotFound />} />
-        <Route path={routes.chatPath()} element={<PrivateRoute />} />
+        <Route path={routes.chatPath()} element={(
+            <PrivateRoute>
+              <ChatPage />
+            </PrivateRoute>
+          )}
+        />
         <Route path={routes.loginPath()} element={<LoginPage />} />
         <Route path={routes.signupPath()} element={<SignupPage />} />
       </Routes>
