69712486eb6f549b37d9407f7bf847ca93efdd09
Aleksandr Manujlo <dregmar@bk.ru>
2024-02-13 20:34:34 +0400

    refactor ChannelBox.js

--- frontend/src/components/ChannelBox.js
+++ frontend/src/components/ChannelBox.js
@@ -14,8 +14,8 @@ const ChannelBox = () => {
   const channelId = useSelector((state) => state.ui.currentChannel);
   const channels = useSelector(channelsSelectors.selectAll);
 
-  const handleOpenModal = (type, e) => {
-    dispatch(openModal({ type, channel: Number(e.target.id)}))
+  const handleOpenModal = (type, channel = null) => {
+    dispatch(openModal({ type, channel }))
   };
 
   const list = channels.map((channel) => {
@@ -61,8 +61,8 @@ const ChannelBox = () => {
           </Dropdown.Toggle>
 
           <Dropdown.Menu>
-            <Dropdown.Item id={channel.id} href="#" onClick={(e) => handleOpenModal('remove', e)}>{t('chatPage.channelBox.remove')}</Dropdown.Item>
-            <Dropdown.Item id={channel.id} href="#" onClick={(e) => handleOpenModal('rename', e)}>{t('chatPage.channelBox.rename')}</Dropdown.Item>
+            <Dropdown.Item id={channel.id} href="#" onClick={() => handleOpenModal('remove', channel.id)}>{t('chatPage.channelBox.remove')}</Dropdown.Item>
+            <Dropdown.Item id={channel.id} href="#" onClick={() => handleOpenModal('rename', channel.id)}>{t('chatPage.channelBox.rename')}</Dropdown.Item>
           </Dropdown.Menu>
         </Dropdown>
       </Nav.Item>
@@ -73,7 +73,7 @@ const ChannelBox = () => {
     <div className="col-4 col-md-2 border-end px-0 bg-light flex-column h-100 d-flex">
       <div className="d-flex mt-1 justify-content-between mb-2 ps-4 pe-2 p-4">
         <b>{t('chatPage.channelBox.channels')}</b>
-        <Button className="p-0 text-primary btn-group-vertical" variant="link" onClick={(e) => handleOpenModal('add', e)}>
+        <Button className="p-0 text-primary btn-group-vertical" variant="link" onClick={() => handleOpenModal('add')}>
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="21" height="21" fill="currentColor">
             <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
             <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
